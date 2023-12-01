3b749b45770e39ca964371d2fc057c286a0c505b
Jonathan.Doan <jonathan.doan@starixsoft.com>
2023-12-01 12:58:14 +0700

    Update page.tsx
    Layout Product

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,6 @@
 'use client'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import useSWR from "swr";
 import Image from "next/image";
 import Link from "next/link";
@@ -23,11 +25,32 @@ const ProductsPage = () => {
     }
 
     return (
-        <main className="row bg-gray-50 pb-8">
+        <Row className='mt-5'>
             {/* <AppTable
                 products={data?.sort((a: any, b: any) => b.id - a.id) ?? []}
             /> */}
-            <section className="grid grid-cols-2 gap-3 my-8 w-[95vw] mx-auto">
+            {data.map((item: IProduct) => (
+                <Col
+                    xs={12} md={4}
+                >
+                    <Link
+                        href={`/${item.id}`}
+                        // className=" shadow-lg hover:shadow-xl"
+                        key={item.id}
+                    >
+                        <div className="">
+                            <Image
+                                src={item.image}
+                                width={400}
+                                height={400}
+                                alt={item.image}
+                                // className="h-full w-full object-cover hover:scale-105 transition-transform"
+                            />
+                        </div>
+                    </Link>
+                </Col>
+            ))}
+            {/* <section className="grid grid-cols-2 gap-3 my-8 w-[95vw] mx-auto">
                 {data.map((item: IProduct) => (
                     <Link
                         href={`/${item.id}`}
@@ -45,8 +68,8 @@ const ProductsPage = () => {
                         </div>
                     </Link>
                 ))}
-            </section>
-        </main>
+            </section> */}
+        </Row>
     )
 }
 
